4f9747837dfeadcaf8cf8d649e37d83cf6f5d035
Deeraj Kumar <dheerajkumarvirat18@msitprogram.net>
2024-03-06 15:57:28 +0530

    error fixed in crawl method

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ class WebCrawler:
                 if href:
                     if urlparse(href).netloc:
                         href = urljoin(base_url or url, href)
-                    if not href.startswith(base_url or url):
+                        #  BUG FOUND IN THE NEXT LINE
+                    # if not href.startswith(base_url or url):
+                        # In this line, NOT keyword is removed.
+                    if href.startswith(base_url or url):
                         self.crawl(href, base_url=base_url or url)
         except Exception as e:
             print(f"Error crawling {url}: {e}")
