5c622cf8c0f520e4e10ff7031b8a50f999a61e11
koribillRamudu <sekharramudu9054@msitprogram.net>
2024-03-06 16:07:04 +0530

    fixed the if condtion in crawl method

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class WebCrawler:
                 if href:
                     if urlparse(href).netloc:
                         href = urljoin(base_url or url, href)
-                    if not href.startswith(base_url or url):
+                    if  href.startswith(base_url or url):   #fixed if condition
                         self.crawl(href, base_url=base_url or url)
         except Exception as e:
             print(f"Error crawling {url}: {e}")
